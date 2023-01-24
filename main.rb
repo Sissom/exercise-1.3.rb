555e412524890022b48610e99b777b7970150d30
Sissom <111325589+Sissom@users.noreply.github.com>
2023-01-24 01:39:54 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,13 @@
+puts "What is the airspeed velocity of an unladen swallow?"
+airspeed = gets.chomp
+puts "You entered #{airspeed}."
+
+puts "What number do you want to multiply by 2?"
+num = gets.chomp
+multiply_by_two = Integer(num) * 2
+puts "#{num} multiplied by 2 is #{multiply_by_two}."
+
+puts "What number do you want to divide by 2?"
+num = gets.chomp
+divide_by_two = Integer(num) / 2
+puts "#{num} divided by 2 is #{divide_by_two}."
